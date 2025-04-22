5cc2c7b87c186cb597d7214a3a9a21990ff6b6c3
lorussogiacomoluca <lorussogiacomoluca@gmail.com>
2025-04-22 15:43:51 +0200

    [UPD] set app.js

--- app.js
+++ app.js
@@ -6,3 +6,11 @@ const app = express()
 
 //app port
 const port = 3000
+
+app.get('/', (req,res)=>{
+    res.send('Il mio blog')
+})
+
+app.listen(port, ()=>{
+    console.log(`In ascolto su porta ${port}`)
+})
