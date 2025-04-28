52d7a956216b8d29886025dbb22e3d50c03fecd4
lorussogiacomoluca <lorussogiacomoluca@gmail.com>
2025-04-28 15:27:53 +0200

    [UPD] Define json object

--- middleware/notFound.js
+++ middleware/notFound.js
@@ -2,4 +2,9 @@ const notFound = function(req, res, next){
     //Set status
     res.status(404)
     
+    //Generate error json object
+    res.json({
+        error: `Pagina non trovata`,
+        message: `Nessuna pagina trovata`
+    })
 }
