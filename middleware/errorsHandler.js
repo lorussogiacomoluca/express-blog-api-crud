4d82842d107ba49fde9fb5daeb9b1b3117adccf4
lorussogiacomoluca <lorussogiacomoluca@gmail.com>
2025-04-28 15:34:39 +0200

    [UPD] errorHandler middleware generate error message with json object

--- middleware/errorsHandler.js
+++ middleware/errorsHandler.js
@@ -2,4 +2,9 @@ const errorHandler = function(err, req, res, next){
     //Set status
     res.status(500)
 
+    //Generate message with json object
+    res.json({
+        error: err.message
+    })
+
 }
